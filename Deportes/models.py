df4487b5a5016751dcbe43928ccdaf8450a7c850
FernandoSka <yayo280@hotmail.com>
2015-10-29 02:05:45 -0600

    modelos

--- Deportes/models.py
+++ Deportes/models.py
@@ -1,3 +1,27 @@
 from django.db import models
-#prueba de pull
+# prueba de pull
 # Create your models here.
+
+
+class Deporte(models.Deporte):
+    """docstring for deporte"""
+    dpr_info = models.CharField(max_length="30")
+
+
+class Amigos(models.Amigos):
+    """docstring for Amigos"""
+    usr_id1 = models.IntegerField(null=False, blank=True)
+    usr_id2 = models.IntegerField(null=False, blank=True)
+
+
+class Actividad(object):
+    """docstring for Actividad"""
+    clb_id = models.IntegerField(null=False, blank=True)
+    usr_id = models.IntegerField(null=False, blank=True)
+    act_info = models.CharField(max_length="500")
+
+
+class DeporteUsuario(object):
+    """docstring for Deporte-Usuario"""
+    dpr_id = models.IntegerField(null=False, blank=True)
+    usr_id = models.IntegerField(null=False, blank=True)
